2d47449af6fbcacf3f54c50b81f9d9396693ccf8
Vladyslav Pustovalov <pustovalov.v.y@gmail.com>
2022-11-27 13:31:08 +0200

    Adding other four Java classes from homework

---
+++ src/FourthClass.java
@@ -0,0 +1,5 @@
+public class FourthClass {
+    public static void main(String[] args) {
+        System.out.println("My fourth Java class");
+    }
+}
---
+++ src/ThirdClass.java
@@ -0,0 +1,5 @@
+public class ThirdClass {
+    public static void main(String[] args) {
+        System.out.println("My third Java class");
+    }
+}

ada46c99083be0438773dcefc42e5eccbf17973f
Vladyslav Pustovalov <pustovalov.v.y@gmail.com>
2022-12-04 15:10:05 +0200

    Finished Bank class with all exercises

--- src/SecondHomework/Bank.java
+++ src/SecondHomework/Bank.java
@@ -1,15 +1,22 @@
 package SecondHomework;
 
+import java.util.Objects;
+
 public class Bank {
     static double balance, withdrawal;
     static String ownerName;
+    static int[] balances = {1200, 250, 2000, 500, 3200};
+    static String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
     public static void main(String[] args) {
         balance = 100;
         withdrawal = 90;
         System.out.println(withdrawingMoneyFromAccount());
         ownerName = "Ann";
-        withdrawal = 250;
+        withdrawal = 50;
         System.out.println(checkingWithdrawingMoney());
+        ownerName = "Ann";
+        withdrawal = 550;
+        System.out.println(balanceFunding());
     }
     static String withdrawingMoneyFromAccount(){
         double commission, newBalance;
@@ -24,14 +31,12 @@ public class Bank {
         return result;
     }
     static String checkingWithdrawingMoney() {
-        int[] balances = {1200, 250, 2000, 500, 3200};
-        String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
         String result;
         int i;
         i = 0;
         double commission, newBalance;
         commission = withdrawal * 0.05;
-        while (ownerName != ownerNames[i]) {
+        while (!Objects.equals(ownerName, ownerNames[i])) {
             i++;
         }
         newBalance = balances[i] - (withdrawal + commission);
@@ -42,5 +47,15 @@ public class Bank {
         }
         return result;
     }
-
+    static String balanceFunding() {
+        String result;
+        int i;
+        i = 0;
+        while (!Objects.equals(ownerName, ownerNames[i])) {
+            i++;
+        }
+        balances[i] = (int) (balances[i] + withdrawal);
+        result = ownerNames[i] + " " + balances[i];
+        return result;
+    }
 }

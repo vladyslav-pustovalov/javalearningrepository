1904f17ada70e1811cf1e973a78d70d675313fca
Vladyslav Pustovalov <pustovalov.v.y@gmail.com>
2022-12-10 17:31:25 +0200

    Finished Adder class

--- src/thirdhomework/Adder.java
+++ src/thirdhomework/Adder.java
@@ -2,7 +2,11 @@ package thirdhomework;
 
 public class Adder extends Arithmetic{
     public static void main(String[] args) {
-
+        System.out.println(check(3,2));
     }
-
+    static boolean check (int a, int b) {
+        boolean result;
+        result = a >= b;
+        return result;
+    }
 }

8e39808af5e903cc2e9a30634bc32995df34763a
Vladyslav Pustovalov <pustovalov.v.y@gmail.com>
2022-12-10 17:25:25 +0200

    Changing in Arithmetic class and creating Adder class

---
+++ src/thirdhomework/Adder.java
@@ -0,0 +1,8 @@
+package thirdhomework;
+
+public class Adder extends Arithmetic{
+    public static void main(String[] args) {
+
+    }
+
+}
--- src/thirdhomework/Arithmetic.java
+++ src/thirdhomework/Arithmetic.java
@@ -2,15 +2,13 @@ package thirdhomework;
 
 public class Arithmetic {
     public static void main(String[] args) {
-        System.out.println(add(new int[]{1,2,3,4}));
-        System.out.println(add(new int[]{5,6,7,8,9,10}));
+        System.out.println(add(1,2));
+        System.out.println(add(3,4));
+        System.out.println(add(5,6));
     }
-    static int add(int... args) {
-        int i, sum;
-        sum = 0;
-        for (i = 0; i < args.length; i++) {
-            sum += args[i];
-        }
+    static int add(int a, int b) {
+        int sum;
+        sum = a + b;
         return sum;
     }
 }

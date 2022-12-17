b38526d66fd5e5b555fc7e737367dca6f992be97
Vladyslav Pustovalov <pustovalov.v.y@gmail.com>
2022-12-17 18:38:02 +0200

    task one

---
+++ src/fourthhomework/Bank.java
@@ -0,0 +1,79 @@
+package fourthhomework;
+
+public abstract class Bank {
+
+    long id;
+    String bankCountry;
+    //Currency currency;
+    int numberOfEmployees;
+    double avrSalaryOfEmployee;
+    long rating;
+    long totalCapital;
+
+    public Bank(long id, String bankCountry, int numberOfEmployees, double avrSalaryOfEmployee, long rating, long totalCapital) {
+        this.id = id;
+        this.bankCountry = bankCountry;
+        this.numberOfEmployees = numberOfEmployees;
+        this.avrSalaryOfEmployee = avrSalaryOfEmployee;
+        this.rating = rating;
+        this.totalCapital = totalCapital;
+    }
+
+    abstract int getLimitOfWithdrawal();
+
+    abstract int getLimitOfFunding();
+
+    abstract int getMonthlyRate();
+
+    abstract int getCommission(int sum);
+
+    abstract double moneyPaidMonthlyForSalary();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBankCountry() {
+        return bankCountry;
+    }
+
+    public void setBankCountry(String bankCountry) {
+        this.bankCountry = bankCountry;
+    }
+
+    public int getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(int numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public double getAvrSalaryOfEmployee() {
+        return avrSalaryOfEmployee;
+    }
+
+    public void setAvrSalaryOfEmployee(double avrSalaryOfEmployee) {
+        this.avrSalaryOfEmployee = avrSalaryOfEmployee;
+    }
+
+    public long getRating() {
+        return rating;
+    }
+
+    public void setRating(long rating) {
+        this.rating = rating;
+    }
+
+    public long getTotalCapital() {
+        return totalCapital;
+    }
+
+    public void setTotalCapital(long totalCapital) {
+        this.totalCapital = totalCapital;
+    }
+}
